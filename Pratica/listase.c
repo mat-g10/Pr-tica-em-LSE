2a630aaa4c6fb73080f37fefc4a6a07f019a0265
Matheus Gab <154209152+mat-g10@users.noreply.github.com>
2024-05-15 16:52:39 -0400

    Update listase.c

--- Pratica/listase.c
+++ Pratica/listase.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "stdio.h"
+#include "stdlib.h"
 
 typedef struct elem_lse{
 
@@ -13,8 +13,8 @@ typedef struct elem_lse{
 
 typedef struct lse{
 
-    struct t_elemento_lse* inicio;
-    struct t_elemento_lse* fim;
+    t_elemento_lse* inicio;
+    t_elemento_lse* fim;
 
     int tamanho;
 
@@ -32,31 +32,7 @@ t_elemento_lse* criarElemento(int id, double pe, double al){
 
 }
 
-t_lse* criarListaSE(){
-
-    t_lse* lista = malloc(sizeof(t_lse));
-
-    lista->inicio = NULL;
-    lista->fim = NULL;
-    lista->tamanho = 0;
-
-    return lista;
-
-}
-
-void inserirInicio(t_lse* lse, int id, double pe, double al){
-    t_elemento_lse* novo = criarElemento(id, pe, al);
-
-    if(lse->inicio == NULL){
-        lse->inicio = novo;
-        lse->fim = novo;
-    }
-    else{
-        novo->prox = lse->inicio;
-        lse->inicio = novo;
-    }
-    lse->tamanho++;
-}
+// inserir_final_lse
 
 void inserirFinal(t_lse* lse, int id, double pe, double al){
     t_elemento_lse* novo = criarElemento(id, pe, al);
@@ -73,8 +49,96 @@ void inserirFinal(t_lse* lse, int id, double pe, double al){
 }
 
 
+// remover_inicio_lse
 
-int main(void){
+typedef struct{
+    int idade;
+    double peso;
+    double altura;
 
+}retornoRemover;
+
+
+retornoRemover remover_elemento_inicio(t_lse* lse){
+
+    t_elemento_lse* aux = lse->inicio;
+    lse->inicio = aux->prox;
+
+    retornoRemover retorno;
+    retorno.idade = aux->idade;
+    retorno.peso = aux->peso;
+    retorno.altura = aux->altura;
+    lse->tamanho--;
+    free(aux);
+
+    
+    return retorno;
+
+}
+
+// tamanho_lse
+
+// criar_lse
+
+t_lse* criarListaSE(){
+
+    t_lse* lista = malloc(sizeof(t_lse));
+
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->tamanho = 0;
+
+    return lista;
+
+}
+
+// implemente aqui a entrada de dados
+// para testar seu código
+int main(int argc, char const *argv[])
+{
+    t_lse* lista = criarListaSE();
+    int num = 1;
+    int id;
+    double pe, al;
+
+    while(num == 1){
+        int opcao;
+        scanf("%d",&opcao);
+
+        switch (opcao)
+        {
+        case 1:  //Inserir No Final
+            if(lista){
+                scanf("%d",&id);
+                scanf("%lf",&pe);
+                scanf("%lf",&al);
+                inserirFinal(lista, id, pe, al);
+            }
+            break;
+
+        case 2: //Remover do Inicio
+            if(lista->tamanho > 0){
+                retornoRemover result = remover_elemento_inicio(lista);
+                printf("%d ",result.idade);
+                printf("%.1lf ",result.peso);
+                printf("%.1lf\n",result.altura);
+            }
+            break;
+
+        case 3: //Tamanho Da Lista
+            if(lista){
+                printf("%d\n",lista->tamanho);
+            }
+            break;
+
+        case 0:
+            num = 0;
+            break;
+
+        default:
+            num = 0;
+            break;
+        }
+    }
     return 0;
 }
